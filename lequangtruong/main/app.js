528488a0d65b2612a033aec97727f98450df1724
quangtruong3011 <lequangtruong3011@gmail.com>
2023-04-17 23:29:16 +0700

    update

--- lequangtruong/main/app.js
+++ lequangtruong/main/app.js
@@ -44,3 +44,21 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// Scoll top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btn-scroll-top").classList.remove("hidden");
+  } else {
+    document.getElementById("btn-scroll-top").classList.add("hidden");
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+document.getElementById("btn-scroll-top").addEventListener("click", topFunction);
